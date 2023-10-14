9f4e3b1d633073edd40a9b692738219bba7aa749
dev-alihasan <infomeorsendmehelp@protonmail.com>
2023-10-15 00:07:34 +0600

    variable

---
+++ variables/script.js
@@ -0,0 +1,23 @@
+/* we use variable as like a box so we can store data or anything 
+there are 3 typs of keywords that can use as a variable
+
+1) var. -- var is old style variable declaration.
+2) let. -- let is modern variable declaration.
+3) const.-- const is like let but we cann't changed value. 
+
+*/
+
+/* so now we'r creating a varibale using let*/
+
+let gf; /* we create a varible with the name gf, so here gf is our variable name*/
+
+/* we can put some into it by using assignment = oparetor*/
+
+gf =
+  "adisa"; /* so my gf name is now saved into gf named variable, me or we can access it anytime by using that variable name*/
+
+console.log(gf); /* adisa */
+
+/** we can combine the variable declaration and assignment into a single line:*/
+
+let gf = "adisa";
